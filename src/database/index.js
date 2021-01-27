24158c8c7170de3d4085cccfff983863659a0bb2
Rennan <rennandamiao@gmail.com>
2021-01-26 23:08:33 -0300

    parte1 final

--- src/database/index.js
+++ src/database/index.js
@@ -1,2 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb:')
+mongoose.connect('mongodb+srv://rennan:<password>@cluster0.bq15o.mongodb.net/<dbname>?retryWrites=true&w=majority',{useMongoClient:true});
+mongoose.Promise = global.Promise;
+module.exports = mongoose;
