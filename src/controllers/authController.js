4b989f37ad91a123a921f2dbf3595f6b30b61cc2
Rennan <rennandamiao@gmail.com>
2021-01-28 21:15:33 -0300

    2,1

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require ('jsonwebtoken');
+
 const User = require('../models/User');
+
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
@@ -18,4 +22,22 @@ router.post('/register', async (req, res) => {
   }
 });
 
+router.post('/autenticate',async(req,res)=>{
+    const {email,password}=req.body;
+
+    const user = await User.findOne({email}).select('+password');
+
+    if (!user){
+        return res.status(400).send({error:'user not found'});
+    }
+
+    if (!await bcrypt.compare(password, user.password)){
+        return res.status(400).send({error:'password incorret , login is failed '});
+    }    
+    user.password=undefined;
+    const token = jwt.sign({id=user.id}, )
+
+    res.send({user});
+
+});
 module.exports = (app) => app.use('/auth', router);
--- src/index.js
+++ src/index.js
@@ -5,11 +5,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
-/*
-app.get('/',(req,res)=>{
-res.send('ok');
-});
-*/
+
 require('./controllers/authController')(app);
 
 app.listen(3000);
